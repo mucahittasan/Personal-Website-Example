a0c2937bad940111cb220fe5794e36bcf8515b6f
Mücahit <88967412+mucahittasan@users.noreply.github.com>
2022-05-13 15:10:16 +0300

    Aos is removed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,17 @@
 import contactImage from "../images/contact.png";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import { useEffect } from "react";
 
 const Contact = () => {
-  useEffect(() => {
-    AOS.init({ duration: 1500 });
-  }, []);
-
   return (
     <section className="contact" id="contact">
       <div className="container">
-        <h2 data-aos="fade-down" className="main-header">
+        <h2 className="main-header">
           <span>{"<"}</span>
           Contact
           <span>{">"}</span>
         </h2>
 
         <div className="contact-content">
-          <form data-aos="zoom-in">
+          <form>
             <div className="form-content">
               <div className="form-info">
                 <input type="text" placeholder="Fullname" />
@@ -31,7 +24,7 @@ const Contact = () => {
             </div>
             <button className="btn-primary">Send</button>
           </form>
-          <div data-aos="fade-up" className="contact-image">
+          <div className="contact-image">
             <img src={contactImage} alt="contactImage" />
           </div>
         </div>
