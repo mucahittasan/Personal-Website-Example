d8616eb0f598e6914933e71c0cd0fb55c5f62dad
Mücahit <88967412+mucahittasan@users.noreply.github.com>
2022-05-13 15:09:16 +0300

    Data aos is removed

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,16 @@
-import React, { useEffect } from "react";
 import skills from "../skills";
-import AOS from "aos";
-import "aos/dist/aos.css";
 
 const About = () => {
-  useEffect(() => {
-    AOS.init({ duration: 1500 });
-  }, []);
-
   return (
     <section className="about" id="about">
-      <h2 data-aos="fade-up" className="main-header">
+      <h2 className="main-header">
         <span>{"<"}</span>
         About
         <span>{">"}</span>
       </h2>
 
       <div className="about-content container">
-        <p data-aos="zoom-in" className="description">
+        <p className="description">
           Hey there <span>👋</span> , I'm Mucahit. If I have to talk about
           myself briefly, I can say that I am determined, hardworking, ambitious
           and able to produce fast solutions. I love teaching and learning. I
@@ -27,7 +20,7 @@ const About = () => {
 
 
 
-        <h3 data-aos="fade-down" className="skills-header">
+        <h3 className="skills-header">
           <span>{"<"}</span>
           Skills
           <span>{">"}</span>
@@ -35,7 +28,7 @@ const About = () => {
         </h3>
         <div className="skills">
           {skills.map((skill, index) => (
-            <div data-aos="zoom-in" key={index} className="skil">
+            <div key={index} className="skil">
               <div className="skill-title">
                 <div>{skill.name}</div>
                 <div>{skill.per}</div>
